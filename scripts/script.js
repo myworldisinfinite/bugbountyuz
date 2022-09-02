ffb41fb226c29e6b83553d423631fdb18fd0226a
myworldisinfinite <myworldisinfinite@gmail.com>
2022-09-02 20:49:38 +0500

    #

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,27 @@ console.log("This program is running");
 
 let links = document.querySelectorAll(".nav-link");
 
-links.forEach((item) => {
+links.forEach(item => {
     item.addEventListener('click', (e) => {
         
-        links.forEach((others) => {
+        links.forEach(others => {
             others.classList.remove('active');       
         });
 
         item.classList.add('active');       
     })
  });
+
+
+let faqRows = document.querySelectorAll(".question");
+
+faqRows.forEach(faqRow => {
+    faqRow.addEventListener("click", () => {
+        faqRow.classList.toggle('closed');
+        
+    })
+})
+
 
 //  item.classList.toggle('active');
 //  item.classList.add('active');
