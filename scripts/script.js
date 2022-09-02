aee7d051c787426da298ea1b22c93c49255db58c
myworldisinfinite <myworldisinfinite@gmail.com>
2022-09-02 19:18:40 +0500

    #

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 console.log("This program is running");
+
+let links = document.querySelectorAll(".nav-link");
+
+links.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        
+        links.forEach((others) => {
+            others.classList.remove('active');       
+        });
+
+        item.classList.add('active');       
+    })
+ });
+
+//  item.classList.toggle('active');
+//  item.classList.add('active');
